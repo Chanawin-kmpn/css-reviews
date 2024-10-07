27d818a5952e37bc2dac2d1311104c4e798c2018
Pond <pchanawin@gmail.com>
2024-10-07 13:59:53 +0700

    add content each page

---
+++ app/fundamental/anatomy-of-a-style-rule/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>ส่วนประกอบของ CSS</div>;
+};
+
+export default page;
---
+++ constant/index.ts
@@ -0,0 +1,76 @@
+export const contents = [
+  {
+    content_Title: "พื้นฐาน CSS",
+    content_Link: "/fundamental",
+    subcontents: [
+      {
+        subcontent_Title: "Anatomy of a Style Rule",
+        subcontent_link: "/fundamental/anatomy-of-a-style-rule",
+      },
+      {
+        subcontent_Title: "Selectors และ Pseudo-classes",
+        subcontent_link: "/fundamental/selectors-and-pseudo-classes",
+      },
+      {
+        subcontent_Title: "The Cascade",
+        subcontent_link: "/fundamental/the-cascade",
+      },
+      {
+        subcontent_Title: "Box Model",
+        subcontent_link: "/fundamental/box-model",
+      },
+      {
+        subcontent_Title: "Units",
+        subcontent_link: "/fundamental/units",
+      },
+      {
+        subcontent_Title: "Typography",
+        subcontent_link: "/fundamental/typography",
+      },
+    ],
+  },
+  {
+    content_Title: "Layout",
+    content_Link: "/layout",
+    subcontents: [
+      {
+        subcontent_Title: "Flow Layout",
+        subcontent_link: "/layout/flow-layout",
+      },
+      {
+        subcontent_Title: "Flexbox",
+        subcontent_link: "/layout/flexbox",
+      },
+      {
+        subcontent_Title: "CSS Grid",
+        subcontent_link: "/layout/css-grid",
+      },
+      {
+        subcontent_Title: "Positioning (Relative, Absolute, Fixed, Sticky)",
+        subcontent_link: "/layout/positioning",
+      },
+    ],
+  },
+  {
+    content_Title: "Responsive Design",
+    content_Link: "/responsive-design",
+    subcontents: [
+      {
+        subcontent_Title: "Media Queries",
+        subcontent_link: "/responsive-design/media-queries",
+      },
+      {
+        subcontent_Title: "Viewport Units",
+        subcontent_link: "/responsive-design/viewport-units",
+      },
+      {
+        subcontent_Title: "Fluid Typography",
+        subcontent_link: "/responsive-design/fluid-typography",
+      },
+      {
+        subcontent_Title: "Responsive Images",
+        subcontent_link: "/responsive-design/responsive-images",
+      },
+    ],
+  },
+];
